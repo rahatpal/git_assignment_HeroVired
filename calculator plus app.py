9ccfc93d63177a59de95d1cf3e1ab9f9342167d3
rahatpal <rahat.pal@gmail.com>
2026-03-22 04:01:04 +0530

    The square root function has been added

--- calculator plus app.py
+++ calculator plus app.py
@@ -14,6 +14,9 @@ class Calculator:
     def divide(self, a, b):
         return a / b
 
+#Square root function
+    def square_root(self, x):
+        return math.sqrt(x)
 
 
 if __name__ == "__main__":
@@ -26,3 +29,6 @@ if __name__ == "__main__":
     print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}")
     print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
     print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
+
+    num3 = 25
+    print(f"The square root of {num3} = {calculator.square_root(num3)}")
